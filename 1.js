97b0665848185dabe6d5caf55df3538a6fd809d6
YOUSSEF <aitabbou111@gmail.com>
2025-10-17 18:25:21 +0200

    calcul

---
+++ 1.js
@@ -0,0 +1,19 @@
+const prompt=require("prompt-sync")();
+
+
+let i=0;
+while(f!=="quitter" ){
+    let n=Number(prompt("entre le premier nombre :"));
+    let m=Number(prompt("entrer le deuxieme nombre :"));
+    let f=prompt("choisir loperation (+,-,*,/,^,r,!)ou quitter :");
+
+ if(!isNaN(a)&&!isNaN(b) &&f!=="quitter" )
+if(f==="+"){
+    
+        function addition(a,b){
+            
+            return a+b
+                        }
+console.log("le resultat est :",addition(n,m))
+}
+else{console.log("nombre invalide")}}
